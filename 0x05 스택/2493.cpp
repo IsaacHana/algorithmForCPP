d9c1a4dc8c180f58ef9a21a2de39d035d4e0ff08
IsaacHana <isaachan233@gmail.com>
2023-12-23 22:04:40 +0900

    2493 문제 해결

---
+++ 0x05 스택/2493.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    stack<pair<int, int>> S;
+    S.push({100000001, 0});
+    int idx = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int h;
+        cin >> h;
+        while (S.top().first < h)
+            S.pop();
+
+        cout << S.top().second << " ";
+
+        S.push({h, i});
+    }
+}

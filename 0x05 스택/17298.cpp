1f8cc3dca16642c403f45add795cbbbbffd54f38
IsaacHana <isaachan233@gmail.com>
2023-12-26 21:56:13 +0900

    17298 문제 해결

--- 0x05 스택/17298.cpp
+++ 0x05 스택/17298.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[1000000];
-int ans[1000000];
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin >> n;
+
     stack<int> S;
-    for (int i = 0; i < n; i++)
+    int N;
+    cin >> N;
+    int arr[N];
+    int ans[N];
+
+    for (int i = 0; i < N; i++)
+    {
         cin >> arr[i];
-    for (int i = n - 1; i >= 0; i--)
+    }
+    for (int i = N - 1; i >= 0; i--)
     {
         while (!S.empty() && S.top() <= arr[i])
         {
             S.pop();
         }
+
         if (S.empty())
         {
             ans[i] = -1;
@@ -28,7 +33,7 @@ int main()
         }
         S.push(arr[i]);
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
     {
         cout << ans[i] << " ";
     }

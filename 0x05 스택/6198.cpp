4b27cbaced860fdc1153a6604e67955d081b2b32
IsaacHana <isaachan233@gmail.com>
2023-12-26 21:55:48 +0900

    6198 문제 해결

--- 0x05 스택/6198.cpp
+++ 0x05 스택/6198.cpp
@@ -6,22 +6,19 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N;
-    long long ans = 0;
     stack<long long> S;
-
+    long long ans = 0;
+    int N;
     cin >> N;
-
     while (N--)
     {
-        long long h;
+        int h, cnt = 1;
         cin >> h;
 
         while (!S.empty() && S.top() <= h)
         {
             S.pop();
         }
-
         ans += S.size();
 
         S.push(h);

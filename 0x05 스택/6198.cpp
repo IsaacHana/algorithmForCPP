37969b04969c18042ed57cd82f85721a1c114888
IsaacHana <isaachan233@gmail.com>
2023-12-27 07:47:05 +0900

    6198 다시 풀기

--- 0x05 스택/6198.cpp
+++ 0x05 스택/6198.cpp
@@ -6,21 +6,20 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    stack<long long> S;
-    long long ans = 0;
+    stack<int> S;
     int N;
+    long long ans;
+
     cin >> N;
     while (N--)
     {
-        int h, cnt = 1;
+        int h;
         cin >> h;
-
         while (!S.empty() && S.top() <= h)
         {
             S.pop();
         }
         ans += S.size();
-
         S.push(h);
     }
 

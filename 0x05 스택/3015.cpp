34741d2cd685ac9e16afb7f8f6714f311a241c54
IsaacHana <isaachan233@gmail.com>
2023-12-26 21:55:41 +0900

    3015문제 해결

--- 0x05 스택/3015.cpp
+++ 0x05 스택/3015.cpp
@@ -6,14 +6,15 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N;
-    cin >> N;
     stack<pair<int, int>> S;
     long long ans = 0;
+    int N;
+    cin >> N;
     while (N--)
     {
         int h, cnt = 1;
         cin >> h;
+
         while (!S.empty() && S.top().first <= h)
         {
             ans += S.top().second;
@@ -21,13 +22,16 @@ int main()
             {
                 cnt += S.top().second;
             }
+
             S.pop();
         }
         if (!S.empty())
         {
             ans++;
         }
+
         S.push({h, cnt});
     }
+
     cout << ans;
 }

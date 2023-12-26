15eb2002ec670d3b88a2547e3176589c1e858b19
IsaacHana <isaachan233@gmail.com>
2023-12-27 07:47:35 +0900

    17298 다시 풀기

--- 0x05 스택/17298.cpp
+++ 0x05 스택/17298.cpp
@@ -6,7 +6,6 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    stack<int> S;
     int N;
     cin >> N;
     int arr[N];
@@ -16,6 +15,8 @@ int main()
     {
         cin >> arr[i];
     }
+
+    stack<int> S;
     for (int i = N - 1; i >= 0; i--)
     {
         while (!S.empty() && S.top() <= arr[i])
@@ -24,15 +25,14 @@ int main()
         }
 
         if (S.empty())
-        {
             ans[i] = -1;
-        }
         else
         {
             ans[i] = S.top();
         }
         S.push(arr[i]);
     }
+
     for (int i = 0; i < N; i++)
     {
         cout << ans[i] << " ";

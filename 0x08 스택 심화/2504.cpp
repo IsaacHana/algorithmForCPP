648153fb801264d9b55325638d409b192fe47b58
IsaacHana <isaachan233@gmail.com>
2023-12-30 20:29:19 +0900

    2504 문제 다시 풀이

--- 0x08 스택 심화/2504.cpp
+++ 0x08 스택 심화/2504.cpp
@@ -5,53 +5,48 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    long long ans = 0;
     stack<char> S;
-    bool isValid = true;
-    string s;
-    cin >> s;
-    int temp = 1;
+    string str;
+    cin >> str;
 
-    for (int i = 0; i < s.length(); i++) {
-        char c = s[i];
+    long long ans = 0;
+    int num = 1;
+    bool isValid = true;
+
+    for (int i = 0; i < str.length(); i++) {
+        char c = str[i];
 
         if (c == '(') {
-            temp *= 2;
             S.push(c);
+            num *= 2;
         } else if (c == '[') {
-            temp *= 3;
             S.push(c);
-        } else {  // ')' || ']'
-            char prev = s[i - 1];
+            num *= 3;
+        } else {
+            char prev = str[i - 1];
             if (c == ')') {
                 if (S.empty() || S.top() != '(') {
                     isValid = false;
                     break;
                 }
-                if (prev == '(') {
-                    ans += temp;
-                }
-                temp /= 2;
-
+                if (prev == '(') ans += num;
+                num /= 2;
                 S.pop();
-            } else {  // ']'
+            } else if (c == ']') {
                 if (S.empty() || S.top() != '[') {
                     isValid = false;
                     break;
                 }
-
-                if (prev == '[') {
-                    ans += temp;
-                }
-                temp /= 3;
-
+                if (prev == '[') ans += num;
+                num /= 3;
                 S.pop();
             }
         }
     }
 
-    if (!S.empty()) isValid = false;
-
+    if (!S.empty()) {
+        isValid = false;
+    }
     if (isValid) {
         cout << ans;
     } else {

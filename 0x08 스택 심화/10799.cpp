2349b43a55f9a12e07cd4efc789127da002c0170
IsaacHana <isaachan233@gmail.com>
2023-12-30 20:29:12 +0900

    10799 문제 다시 풀이

--- 0x08 스택 심화/10799.cpp
+++ 0x08 스택 심화/10799.cpp
@@ -5,19 +5,23 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    long long ans = 0;
     stack<char> S;
-    string s;
-    cin >> s;
+    string str;
+    cin >> str;
+    long long ans = 0;
 
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '(') {
-            S.push(s[i]);
+    for (int i = 0; i < str.length(); i++) {
+        char c = str[i];
+
+        if (c == '(') {
+            S.push(c);
         } else {  // ')'
+            char prev = str[i - 1];
             S.pop();
-            if (s[i - 1] == '(') {  // lazer
+
+            if (prev == '(') {  // lazer
                 ans += S.size();
-            } else {  // bar
+            } else {
                 ans++;
             }
         }

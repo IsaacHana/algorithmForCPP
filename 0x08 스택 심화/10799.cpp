a3cfcfd58644cb6e08995bfd33d42902394cc560
IsaacHana <isaachan233@gmail.com>
2023-12-31 16:00:31 +0900

    10799 복습

--- 0x08 스택 심화/10799.cpp
+++ 0x08 스택 심화/10799.cpp
@@ -5,23 +5,20 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    long long ans = 0;
     stack<char> S;
     string str;
     cin >> str;
-    long long ans = 0;
-
     for (int i = 0; i < str.length(); i++) {
         char c = str[i];
-
         if (c == '(') {
             S.push(c);
         } else {  // ')'
             char prev = str[i - 1];
             S.pop();
-
             if (prev == '(') {  // lazer
                 ans += S.size();
-            } else {
+            } else {  // bar
                 ans++;
             }
         }

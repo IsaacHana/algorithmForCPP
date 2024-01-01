13d3a71cd9dd403ccf65f7974d997c8566a11d4e
IsaacHana <isaachan233@gmail.com>
2024-01-01 19:28:50 +0900

    1021 복습

--- 0x07 덱/1021.cpp
+++ 0x07 덱/1021.cpp
@@ -5,46 +5,26 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int ans = 0;
     deque<int> DQ;
-    deque<int> DQ2;
     int N, M;
-    long long ans = 0;
     cin >> N >> M;
-
-    for (int i = 1; i <= N; i++) {
-        DQ.push_back(i);
-        DQ2.push_back(i);
-    }
-
+    for (int i = 1; i <= N; i++) DQ.push_back(i);
     while (M--) {
         int val;
         cin >> val;
-
-        while (DQ.front() != val && DQ2.front() != val) {
-            DQ.push_back(DQ.front());
-            DQ.pop_front();
-
-            DQ2.push_front(DQ2.back());
-            DQ2.pop_back();
-
+        int idx = find(DQ.begin(), DQ.end(), val) - DQ.begin();
+        while (DQ.front() != val) {
+            if (idx < DQ.size() - idx) {
+                DQ.push_back(DQ.front());
+                DQ.pop_front();
+            } else {
+                DQ.push_front(DQ.back());
+                DQ.pop_back();
+            }
             ans++;
         }
-
-        if (DQ.front() == val) {
-            DQ.pop_front();
-            DQ2 = DQ;
-        } else {
-            DQ2.pop_front();
-            DQ = DQ2;
-        }
+        DQ.pop_front();
     }
     cout << ans;
 }
-
-// 10 3
-// 2 9 5
-
-// 1 2 3 4 5 6 7 8 9 10
-// 1   3 4 5 6 7 8 9 10  //  + 1
-// 1   3 4 5 6 7 8   10  //  + 3
-// 1   3 4   6 7 8   10  //  + 4

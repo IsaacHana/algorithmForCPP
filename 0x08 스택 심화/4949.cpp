1356ec84fabb12e8fd59bf80a9be5a08a8a4c9a9
IsaacHana <isaachan233@gmail.com>
2024-01-06 10:38:31 +0900

    4949 문제 복습

--- 0x08 스택 심화/4949.cpp
+++ 0x08 스택 심화/4949.cpp
@@ -6,32 +6,34 @@ int main() {
     cin.tie(0);
 
     while (true) {
-        string s;
-        getline(cin, s);
-        if (s == ".") break;
+        string str;
+        getline(cin, str);
+        if (str == ".") break;
 
         stack<char> S;
         bool isValid = true;
-
-        for (char c : s) {
-            if (c == '(' || c == '[') S.push(c);
-            if (c == ')') {
-                if (S.empty() || S.top() != '(') {
-                    isValid = false;
-                    break;
+        for (char c : str) {  // 문자열을 하나씩 보겠다.
+            if (c == '(' || c == '[')
+                S.push(c);  // 여는 괄호라면 stack.push()
+            else {
+                if (c == ')') {
+                    if (S.empty() || S.top() != '(') {  // 2-1, 2-2
+                        isValid = false;
+                        break;
+                    }
+                    S.pop();  // 2-3
                 }
-                S.pop();
-            }
-            if (c == ']') {
-                if (S.empty() || S.top() != '[') {
-                    isValid = false;
-                    break;
+                if (c == ']') {
+                    if (S.empty() || S.top() != '[') {  // 2-1, 2-2
+                        isValid = false;
+                        break;
+                    }
+                    S.pop();  // 2-3
                 }
-                S.pop();
             }
         }
 
-        if (!S.empty()) isValid = false;
+        if (!S.empty()) isValid = false;  // 3
 
         if (isValid)
             cout << "yes\n";

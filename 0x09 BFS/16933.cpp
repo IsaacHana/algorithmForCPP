2587610cb2b42d28927521537b7233078de8cfa1
IsaacHana <isaachan233@gmail.com>
2024-01-12 11:22:12 +0900

    16933 문제 복습

--- 0x09 BFS/16933.cpp
+++ 0x09 BFS/16933.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int INF = 987654321;
 
 string board[1001];
-int dist[11][1001][1001];
+int dist[2][11][1001][1001];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int main() {
@@ -18,14 +18,13 @@ int main() {
         cin >> board[i];
     }
 
-    queue<tuple<int, int, int, int, int>> Q;
-    Q.push({1, K, 0, 0, 1});
-    dist[K][0][0] = 1;
+    queue<tuple<int, int, int, int>> Q;
+    Q.push({1, K, 0, 0});
+    dist[1][K][0][0] = 1;
 
     while (!Q.empty()) {
-        int qsize = Q.size();
-        int isBright, k, x, y, stay;
-        tie(isBright, k, x, y, stay) = Q.front();
+        int isBright, k, x, y;
+        tie(isBright, k, x, y) = Q.front();
         Q.pop();
 
         for (int dir = 0; dir < 4; dir++) {
@@ -34,32 +33,35 @@ int main() {
 
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-            if (board[nx][ny] == '1') {                                   // 벽
-                if (!isBright || k < 1 || dist[k - 1][nx][ny]) continue;  // 밤이거나 횟수 전부 사용했거나 방분했다면 넘겨
-
-                dist[k - 1][nx][ny] = dist[k][x][y] + 1;
-                Q.push({!isBright, k - 1, nx, ny, 1});
+            if (board[nx][ny] == '1') {  // 벽
+                if (k < 1) continue;     // 횟수 전부 사용했거나 방분했다면 넘겨
+                if (!isBright) {         // 밤이면 기다려봐
+                    if (dist[!isBright][k][x][y]) continue;
+                    dist[!isBright][k][x][y] = dist[isBright][k][x][y] + 1;
+                    Q.push({!isBright, k, x, y});
+                } else {
+                    if (dist[!isBright][k - 1][nx][ny]) continue;
+                    dist[!isBright][k - 1][nx][ny] = dist[isBright][k][x][y] + 1;
+                    Q.push({!isBright, k - 1, nx, ny});
+                }
             } else {  // 벽X
-                if (dist[k][nx][ny]) continue;
-                dist[k][nx][ny] = dist[k][x][y] + 1;
-                Q.push({!isBright, k, nx, ny, 1});
+                if (dist[!isBright][k][nx][ny]) continue;
+                dist[!isBright][k][nx][ny] = dist[isBright][k][x][y] + 1;
+                Q.push({!isBright, k, nx, ny});
             }
-        }
-
-        if (stay > 0) {
-            dist[k][x][y] = dist[k][x][y] + 1;
-            Q.push({!isBright, k, x, y, stay - 1});
         }
     }
 
     int ans = INF;
 
-    for (int j = 0; j <= K; j++) {
-        if (dist[j][N - 1][M - 1]) ans = min(ans, dist[j][N - 1][M - 1]);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j <= K; j++) {
+            if (dist[i][j][N - 1][M - 1]) ans = min(ans, dist[i][j][N - 1][M - 1]);
+        }
     }
 
     if (ans == INF)
         cout << -1;
     else
-        cout << ans - 1;
+        cout << ans;
 }

18a3fe4ba36a3bdb9317a7aa475f0a59630ab0e2
IsaacHana <isaachan233@gmail.com>
2024-01-16 12:27:52 +0900

    1697 문제 복습

--- 0x09 BFS/1697.cpp
+++ 0x09 BFS/1697.cpp
@@ -1,28 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dist[100002];
+const int MX = 100'000;
+int dist[MX + 1];
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int N, K;
     cin >> N >> K;
-    fill(dist, dist + 100001, -1);
-    dist[N] = 0;
+
+    fill(dist, dist + MX + 1, -1);
+
     queue<int> Q;
     Q.push(N);
+    dist[N] = 0;
+
     while (dist[K] == -1) {
-        int cur = Q.front();
+        auto cur = Q.front();
         Q.pop();
 
-        for (int nxt : {cur - 1, cur + 1, 2 * cur}) {
-            if (nxt < 0 || nxt > 100000) continue;
+        for (int nxt : {cur - 1, cur + 1, cur * 2}) {
+            if (nxt < 0 || nxt > MX) continue;
             if (dist[nxt] != -1) continue;
             dist[nxt] = dist[cur] + 1;
             Q.push(nxt);
         }
     }
 
-    cout << dist[K];
+    cout << dist[K] << '\n';
 }

e8ae347d5d2fa77b0dc991d4c08f491cb77b4bac
IsaacHana <isaachan233@gmail.com>
2024-01-16 12:14:36 +0900

    2178 문제 복습

--- 0x09 BFS/2178.cpp
+++ 0x09 BFS/2178.cpp
@@ -1,51 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maze[101][101];
-bool vis[101][101];
-int N, M;
+int dist[101][101];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-
-void maze_init() {
-    for (int i = 1; i <= N; i++) {
-        string str;
-        cin >> str;
-        for (int j = 1; j <= M; j++) maze[i][j] = str[j - 1] - '0';
-    }
-}
-
-int bfs() {
-    int ans = 1;
-    queue<pair<int, int>> Q;
-    Q.push({1, 1});
-    vis[1][1] = true;
-
-    while (!Q.empty()) {
-        int qsize = Q.size();
-        for (int i = 0; i < qsize; i++) {
-            pair<int, int> cur = Q.front();
-            Q.pop();
-            if (cur.first == N && cur.second == M) return ans;
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur.first + dx[dir];
-                int ny = cur.second + dy[dir];
-
-                if (nx <= 0 || nx > N || ny <= 0 || ny > M || maze[nx][ny] == 0 || vis[nx][ny]) continue;
-
-                vis[nx][ny] = true;
-                Q.push({nx, ny});
-            }
-        }
-        ans++;
-    }
-}
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> N >> M;
 
-    maze_init();
-    cout << bfs();
+    int N, M;
+    cin >> N >> M;
+    string str;
+    for (int i = 1; i <= N; i++) {
+        cin >> str;
+        for (int j = 1; j <= M; j++) {
+            int val = str[j - 1] - '0';
+            if (val) {
+                dist[i][j] = -1;
+            }
+        }
+    }
+    queue<pair<int, int>> Q;
+    Q.push({1, 1});
+    dist[1][1] = 0;
+    while (!Q.empty()) {
+        auto cur = Q.front();
+        Q.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+
+            if (nx < 1 || nx > N || ny < 1 || ny > M) continue;
+            if (dist[nx][ny] != -1) continue;
+
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
+            Q.push({nx, ny});
+        }
+    }
+    cout << dist[N][M] + 1 << '\n';
 }

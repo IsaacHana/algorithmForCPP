23e9b1ffd4a14406033c544dfec39a9ce94d6f82
IsaacHana <isaachan233@gmail.com>
2024-01-16 11:00:01 +0900

    1926 문제 복습

--- 0x09 BFS/1926.cpp
+++ 0x09 BFS/1926.cpp
@@ -1,58 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[502][502];
-bool vis[502][502];
-int n, m;
-
-int mx_area = 0;
-int cnt = 0;
-
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-void board_init() {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) cin >> board[i][j];
-}
+bool board[501][501];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-void bfs() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (board[i][j] == 0 || vis[i][j]) continue;  // 0 = 공백 , vis = 방문
-            cnt++;                                        // 공백도 아니고 방문한적도 없으니 그림의 갯수 추가
+    int N, M, cnt = 0, mx = 0;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (!board[i][j]) continue;
+            cnt++;
 
             queue<pair<int, int>> Q;
-            vis[i][j] = true;  // 그림의 처음 부분 방문
-            Q.push({i, j});    // 그림의 처음 부분 삽입
-            int area = 0;
+            Q.push({i, j});
+            board[i][j] = false;
+            int tmp = 0;
+
             while (!Q.empty()) {
-                area++;
-                pair<int, int> cur = Q.front();
+                auto cur = Q.front();
                 Q.pop();
 
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = cur.first + dx[dir];
                     int ny = cur.second + dy[dir];
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] || board[nx][ny] == 0) continue;
-                    vis[nx][ny] = true;
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                    if (!board[nx][ny]) continue;
+
+                    board[nx][ny] = false;
                     Q.push({nx, ny});
                 }
+                tmp++;
             }
 
-            mx_area = max(mx_area, area);
+            mx = max(mx, tmp);
         }
     }
-}
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> m;
-
-    board_init();
-
-    bfs();
-
-    cout << cnt << '\n' << mx_area;
+    cout << cnt << '\n';
+    cout << mx << '\n';
 }

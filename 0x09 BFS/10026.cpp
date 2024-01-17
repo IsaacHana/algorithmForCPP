6d8753d33394b031860077e51b6ad3bdcebe2554
IsaacHana <isaachan233@gmail.com>
2024-01-17 16:18:31 +0900

    10026 문제 복습 (2024-01-17)

--- 0x09 BFS/10026.cpp
+++ 0x09 BFS/10026.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool vis[102][102];
-char board[102][102];
+char board[101][101];
+bool vis[101][101];
 int N;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
@@ -10,7 +10,6 @@ int dy[4] = {0, 1, 0, -1};
 void bfs(int i, int j) {
     queue<pair<int, int>> Q;
     Q.push({i, j});
-    vis[i][j] = true;
 
     while (!Q.empty()) {
         auto cur = Q.front();
@@ -21,7 +20,9 @@ void bfs(int i, int j) {
             int ny = cur.second + dy[dir];
 
             if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if (vis[nx][ny] || board[i][j] != board[nx][ny]) continue;
+            if (vis[nx][ny]) continue;
+            if (board[nx][ny] != board[i][j]) continue;
+
             vis[nx][ny] = true;
             Q.push({nx, ny});
         }
@@ -32,9 +33,10 @@ int count_area() {
     int cnt = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (vis[i][j]) continue;
-            cnt++;
-            bfs(i, j);
+            if (!vis[i][j]) {
+                cnt++;
+                bfs(i, j);
+            }
         }
     }
     return cnt;
@@ -45,23 +47,23 @@ int main() {
     cin.tie(0);
 
     cin >> N;
-
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cin >> board[i][j];
         }
     }
 
-    int ans1 = count_area();
+    cout << count_area() << " ";
 
     for (int i = 0; i < N; i++) {
         fill(vis[i], vis[i] + N, false);
+    }
+
+    for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (board[i][j] == 'G') board[i][j] = 'R';
         }
     }
 
-    int ans2 = count_area();
-
-    cout << ans1 << " " << ans2;
+    cout << count_area();
 }

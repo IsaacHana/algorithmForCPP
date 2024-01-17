77579aad183d62d655cd51db3513a4fe0cf9cb50
IsaacHana <isaachan233@gmail.com>
2024-01-17 16:18:16 +0900

    7562 문제 복습 (2024-01-17)

--- 0x09 BFS/7562.cpp
+++ 0x09 BFS/7562.cpp
@@ -1,44 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int dist[301][301];
 int dx[8] = {2, 1, -1, -2, -2, -1, 1, 2};
 int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-int dist[305][305];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int T;
+    int T, l;
     cin >> T;
     while (T--) {
-        int I;
-        cin >> I;
-        for (int i = 0; i < I; i++) {
-            fill(dist[i], dist[i] + I, -1);
+        cin >> l;
+        queue<pair<int, int>> Q;
+        int sx, sy, ex, ey;
+        cin >> sx >> sy;
+        cin >> ex >> ey;
+
+        for (int i = 0; i < l; i++) {
+            fill(dist[i], dist[i] + l, -1);
         }
 
-        int SX, SY;
-        cin >> SX >> SY;
+        Q.push({sx, sy});
+        dist[sx][sy] = 0;
 
-        queue<pair<int, int>> Q;
-        Q.push({SX, SY});
-        dist[SX][SY] = 0;
-
-        int EX, EY;
-        cin >> EX >> EY;
-        while (!Q.empty()) {
-            auto cur = Q.front();
+        while (dist[ex][ey] == -1) {
+            int x, y;
+            tie(x, y) = Q.front();
             Q.pop();
+
             for (int dir = 0; dir < 8; dir++) {
-                int nx = cur.first + dx[dir];
-                int ny = cur.second + dy[dir];
-                if (nx < 0 || nx >= I || ny < 0 || ny >= I) continue;
-                if (dist[nx][ny] >= 0) continue;
-                dist[nx][ny] = dist[cur.first][cur.second] + 1;
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if (nx < 0 || nx >= l || ny < 0 || ny >= l) continue;
+                if (dist[nx][ny] != -1) continue;
+
+                dist[nx][ny] = dist[x][y] + 1;
                 Q.push({nx, ny});
             }
         }
 
-        cout << dist[EX][EY] << '\n';
+        cout << dist[ex][ey] << '\n';
     }
 }

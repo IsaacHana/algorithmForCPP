c3fe5cdd3e745854f694ea4eafae12999b236b82
IsaacHana <isaachan233@gmail.com>
2024-01-17 16:17:45 +0900

    5427 문제 복습 (2024-01-17) 문제 다시 풀것

--- 0x09 BFS/5427.cpp
+++ 0x09 BFS/5427.cpp
@@ -1,78 +1,95 @@
-    #include <bits/stdc++.h>
-    using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-    int f_dist[1002][1002];
-    int s_dist[1002][1002];
-    int dx[4] = {1, 0, -1, 0};
-    int dy[4] = {0, 1, 0, -1};
-    int main() {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
+int f_dist[1001][1001];
+int s_dist[1001][1001];
 
-        int T;
-        cin >> T;
-        while (T--) {
-            int w, h;
-            cin >> h >> w;
-            queue<pair<int, int>> FQ;
-            queue<pair<int, int>> SQ;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-            for (int i = 0; i < w; i++) {
-                fill(f_dist[i], f_dist[i] + h, -1);
-                fill(s_dist[i], s_dist[i] + h, -1);
-                for (int j = 0; j < h; j++) {
-                    char val;
-                    cin >> val;
-                    if (val == '#') {
-                        f_dist[i][j] = 0;
-                        s_dist[i][j] = 0;
-                    }
-                    if (val == '*') {
-                        FQ.push({i, j});
-                        f_dist[i][j] = 0;
-                    }
-                    if (val == '@') {
-                        SQ.push({i, j});
-                        s_dist[i][j] = 0;
-                    }
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int w, h;
+        cin >> w >> h;
+        queue<pair<int, int>> FQ;
+        queue<pair<int, int>> SQ;
+
+        for (int i = 0; i < h; i++) {
+            fill(f_dist[i], f_dist[i] + w, -1);
+            fill(s_dist[i], s_dist[i] + w, -1);
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                char val;
+                cin >> val;
+
+                if (val == '#') {
+                    f_dist[i][j] = 0;
+                    s_dist[i][j] = 0;
+                }
+
+                if (val == '*') {
+                    FQ.push({i, j});
+                    f_dist[i][j] = 0;
+                }
+                if (val == '@') {
+                    SQ.push({i, j});
+                    s_dist[i][j] = 0;
                 }
             }
+        }
 
-            while (!FQ.empty()) {
-                auto cur = FQ.front();
-                FQ.pop();
+        while (!FQ.empty()) {
+            int x, y;
+            tie(x, y) = FQ.front();
+            FQ.pop();
 
-                for (int dir = 0; dir < 4; dir++) {
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
 
-                    if (nx < 0 || nx >= w || ny < 0 || ny >= h) continue;
-                    if (f_dist[nx][ny] >= 0) continue;
-                    f_dist[nx][ny] = f_dist[cur.first][cur.second] + 1;
-                    FQ.push({nx, ny});
-                }
+                if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+                if (f_dist[nx][ny] != -1) continue;
+
+                f_dist[nx][ny] = f_dist[x][y] + 1;
+                FQ.push({nx, ny});
             }
+        }
 
-            bool escape = false;
-            while (!SQ.empty() && !escape) {
-                auto cur = SQ.front();
-                SQ.pop();
+        bool escape = false;
+        int ans = 0;
+        while (!SQ.empty() && !escape) {
+            int x, y;
+            tie(x, y) = SQ.front();
+            SQ.pop();
 
-                for (int dir = 0; dir < 4; dir++) {
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
-                    int exp = s_dist[cur.first][cur.second] + 1;
-                    if (nx < 0 || nx >= w || ny < 0 || ny >= h) {
-                        escape = true;
-                        cout << exp << '\n';
-                        break;
-                    }
-                    if (s_dist[nx][ny] >= 0) continue;
-                    if (f_dist[nx][ny] != -1 && f_dist[nx][ny] <= exp) continue;
-                    s_dist[nx][ny] = exp;
-                    SQ.push({nx, ny});
-                }
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
+                    ans = s_dist[x][y] + 1;
+                    escape = true;
+                    break;
+                };
+                if (s_dist[nx][ny] != -1) continue;
+                if (f_dist[nx][ny] != -1 && s_dist[x][y] + 1 >= f_dist[nx][ny]) continue;
+
+                s_dist[nx][ny] = s_dist[x][y] + 1;
+                SQ.push({nx, ny});
             }
-            if (!escape) cout << "IMPOSSIBLE\n";
+        }
+
+        if (escape) {
+            cout << ans << '\n';
+        } else {
+            cout << "IMPOSSIBLE" << '\n';
         }
     }
+}

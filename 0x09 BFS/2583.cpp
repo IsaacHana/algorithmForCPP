a7784c22ec7ccafef4871bf49149e98761f78173
IsaacHana <isaachan233@gmail.com>
2024-01-18 14:52:16 +0900

    2583 문제 복습 (2024. 01. 18)

--- 0x09 BFS/2583.cpp
+++ 0x09 BFS/2583.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool vis[101][101];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -10,55 +11,52 @@ int main() {
 
     int M, N, K;
     cin >> M >> N >> K;
-    int board[M][N] = {};
 
+    int cnt = 0;
+    int sx, sy, ex, ey;
+    queue<pair<int, int>> Q;
+    priority_queue<int, vector<int>, greater<int>> PQ;
     while (K--) {
-        int SX, SY, EX, EY;
-        cin >> SX >> SY >> EX >> EY;
+        cin >> sx >> sy >> ex >> ey;
 
-        for (int i = SY; i < EY; i++) {
-            for (int j = SX; j < EX; j++) {
-                board[i][j] = 1;
+        for (int i = sy; i < ey; i++) {
+            for (int j = sx; j < ex; j++) {
+                vis[i][j] = true;
             }
         }
     }
 
-    priority_queue<int, vector<int>, greater<int>> area_Q;
-
-    int cnt = 0;
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
-            if (board[i][j] == 1) continue;
+            if (vis[i][j]) continue;
             cnt++;
 
-            queue<pair<int, int>> Q;
             Q.push({i, j});
-            board[i][j] = 1;
-
+            vis[i][j] = true;
             int area = 0;
             while (!Q.empty()) {
                 auto cur = Q.front();
                 Q.pop();
+                area++;
 
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = cur.first + dx[dir];
                     int ny = cur.second + dy[dir];
 
                     if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-                    if (board[nx][ny] == 1) continue;
+                    if (vis[nx][ny]) continue;
 
-                    board[nx][ny] = 1;
+                    vis[nx][ny] = true;
                     Q.push({nx, ny});
                 }
-                area++;
             }
-            area_Q.push(area);
+            PQ.push(area);
         }
     }
 
     cout << cnt << '\n';
-    while (!area_Q.empty()) {
-        cout << area_Q.top() << " ";
-        area_Q.pop();
+    while (!PQ.empty()) {
+        cout << PQ.top() << " ";
+        PQ.pop();
     }
 }

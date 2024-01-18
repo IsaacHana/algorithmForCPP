13f432424f2c8190cbbfb2915ba0b78ec354c9da
IsaacHana <isaachan233@gmail.com>
2024-01-18 14:52:37 +0900

    6593 문제 복습 (2024. 01. 18)

--- 0x09 BFS/6593.cpp
+++ 0x09 BFS/6593.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string SBB[32][32];
-int dist[32][32][32];
+int dist[31][31][31];
+
 int dx[6] = {1, 0, 0, -1, 0, 0};
 int dy[6] = {0, 1, 0, 0, -1, 0};
 int dz[6] = {0, 0, 1, 0, 0, -1};
@@ -14,64 +14,66 @@ int main() {
     while (true) {
         int L, R, C;
         cin >> L >> R >> C;
-        if (L == 0 && R == 0 & C == 0) break;
+        if (L == 0 && R == 0 && C == 0) break;
 
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < R; j++) {
-                cin >> SBB[i][j];
+                fill(dist[i][j], dist[i][j] + C, -1);
             }
         }
 
         queue<tuple<int, int, int>> Q;
-        tuple<int, int, int> end;
-
+        int ez, ex, ey;
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < R; j++) {
+                string str;
+                cin >> str;
                 for (int k = 0; k < C; k++) {
-                    char c = SBB[i][j][k];
-                    if (c == 'S') {
+                    int val = str[k];
+                    if (val == '#') {
+                        dist[i][j][k] = 0;
+                    }
+                    if (val == 'S') {
                         Q.push({i, j, k});
                         dist[i][j][k] = 0;
-                    } else if (c == 'E') {
-                        end = {i, j, k};
-                        dist[i][j][k] = -1;
-                    } else if (c == '#') {
-                        dist[i][j][k] = 0;
-                    } else if (c == '.') {
-                        dist[i][j][k] = -1;
+                    }
+                    if (val == 'E') {
+                        ez = i;
+                        ex = j;
+                        ey = k;
                     }
                 }
             }
         }
+
         bool escape = false;
         while (!Q.empty() && !escape) {
-            auto cur = Q.front();
+            int z, x, y;
+            tie(z, x, y) = Q.front();
             Q.pop();
-
-            int x, y, z;
-            tie(x, y, z) = cur;
 
             for (int dir = 0; dir < 6; dir++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
                 int nz = z + dz[dir];
 
-                if (nx < 0 || nx >= L || ny < 0 || ny >= R || nz < 0 || nz >= C) continue;
-                if (dist[nx][ny][nz] != -1) continue;
+                if (nz < 0 || nz >= L || nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+                if (dist[nz][nx][ny] != -1) continue;
 
-                dist[nx][ny][nz] = dist[x][y][z] + 1;
-                Q.push({nx, ny, nz});
+                dist[nz][nx][ny] = dist[z][x][y] + 1;
 
-                if (SBB[nx][ny][nz] == 'E') {
+                if (nz == ez && nx == ex && ny == ey) {
                     escape = true;
                     break;
                 }
+
+                Q.push({nz, nx, ny});
             }
         }
 
         if (escape)
-            cout << "Escaped in " << dist[get<0>(end)][get<1>(end)][get<2>(end)] << " minute(s).\n";
+            cout << "Escaped in " << dist[ez][ex][ey] << " minute(s)." << '\n';
         else
-            cout << "Trapped!\n";
+            cout << "Trapped!" << '\n';
     }
 }

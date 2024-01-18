75446f855954c5421c8b965f1e1b98f3a46434a3
IsaacHana <isaachan233@gmail.com>
2024-01-18 14:52:22 +0900

    2667 문제 복습 (2024. 01. 18)

--- 0x09 BFS/2667.cpp
+++ 0x09 BFS/2667.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string board[26];
+bool vis[26][26];
+
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int main() {
@@ -9,47 +12,45 @@ int main() {
 
     int N;
     cin >> N;
-    int board[N][N] = {};
 
+    queue<pair<int, int>> Q1;
     for (int i = 0; i < N; i++) {
-        string str;
-        cin >> str;
+        cin >> board[i];
         for (int j = 0; j < N; j++) {
-            board[i][j] = str[j] - '0';
+            if (board[i][j] == '1') {
+                Q1.push({i, j});
+            }
         }
     }
-
     int cnt = 0;
     priority_queue<int, vector<int>, greater<int>> PQ;
+    while (!Q1.empty()) {
+        auto cur1 = Q1.front();
+        Q1.pop();
+        if (vis[cur1.first][cur1.second]) continue;
+        cnt++;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (board[i][j] == 0) continue;
-            cnt++;
+        queue<pair<int, int>> Q2;
+        vis[cur1.first][cur1.second] = true;
+        Q2.push({cur1.first, cur1.second});
+        int area = 0;
+        while (!Q2.empty()) {
+            auto cur2 = Q2.front();
+            Q2.pop();
+            area++;
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur2.first + dx[dir];
+                int ny = cur2.second + dy[dir];
 
-            queue<pair<int, int>> Q;
-            Q.push({i, j});
-            board[i][j] = 0;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                if (board[nx][ny] == '0') continue;
+                if (vis[nx][ny]) continue;
 
-            int area = 0;
-            while (!Q.empty()) {
-                auto cur = Q.front();
-                Q.pop();
-
-                for (int dir = 0; dir < 4; dir++) {
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
-
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                    if (board[nx][ny] == 0) continue;
-
-                    board[nx][ny] = 0;
-                    Q.push({nx, ny});
-                }
-                area++;
+                vis[nx][ny] = true;
+                Q2.push({nx, ny});
             }
-            PQ.push(area);
         }
+        PQ.push(area);
     }
 
     cout << cnt << '\n';

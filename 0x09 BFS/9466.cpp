ae1008383a4e617dbc6acad276fc92a7be2afb6c
IsaacHana <isaachan233@gmail.com>
2024-01-19 20:41:06 +0900

    9366 문제 복습 반드시 다시  풀 것! (2024. 01. 19)

--- 0x09 BFS/9466.cpp
+++ 0x09 BFS/9466.cpp
@@ -1,48 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int CYCLE = -1;
+const int MX = 100'000;
+int board[MX + 1];
+int vis[MX + 1];
 
-int board[100001];
-int vis[100001];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int T;
     cin >> T;
-
     while (T--) {
-        int N;
-        cin >> N;
+        int n;
+        cin >> n;
 
-        fill(vis + 1, vis + N + 1, 0);
-
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             cin >> board[i];
         }
 
-        int ans = N;
-        for (int i = 1; i <= N; i++) {
+        int cnt = n;
+
+        for (int i = 1; i <= n; i++) {
             if (vis[i]) continue;
             int cur = i;
-            int cnt = 0;
             while (true) {
                 vis[cur] = i;
                 cur = board[cur];
+
                 if (vis[cur] == i) {
-                    while (vis[cur] != CYCLE) {
-                        cnt++;
-                        vis[cur] = CYCLE;
+                    while (vis[cur] != -1) {
+                        cnt--;
+                        vis[cur] = -1;
                         cur = board[cur];
                     }
                     break;
-                } else if (vis[cur]) {
-                    break;
                 }
+                if (vis[cur]) break;
             }
-            ans -= cnt;
         }
-        cout << ans << '\n';
+
+        cout << cnt << '\n';
+
+        fill(vis + 1, vis + n + 1, 0);
     }
 }

7c16b193432057f6f8022883ad460b666fed8e7f
IsaacHana <isaachan233@gmail.com>
2024-01-22 17:56:59 +0900

    2146 문제 복습 (2024. 01. 22)

--- 0x09 BFS/2146.cpp
+++ 0x09 BFS/2146.cpp
@@ -11,72 +11,80 @@ int dy[4] = {0, 1, 0, -1};
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int N;
     cin >> N;
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cin >> board[i][j];
         }
     }
 
-    int ans = 987654321;
-
+    int idx = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (board[i][j] == 0 || vis[i][j]) continue;
+            idx++;
 
             queue<pair<int, int>> Q;
-            queue<pair<int, int>> GQ;
-
             Q.push({i, j});
-            vis[i][j] = true;
+
             while (!Q.empty()) {
                 auto cur = Q.front();
                 Q.pop();
+                board[cur.first][cur.second] = idx;
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = cur.first + dx[dir];
                     int ny = cur.second + dy[dir];
+
                     if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
                     if (vis[nx][ny]) continue;
-                    if (board[nx][ny] == 0 && !vis[nx][ny]) {
-                        GQ.push({cur.first, cur.second});
-                        continue;
-                    }
+                    if (board[nx][ny] == 0) continue;
+
                     vis[nx][ny] = true;
                     Q.push({nx, ny});
                 }
             }
-            bool complete = false;
-            while (!GQ.empty()) {
-                int qsize = GQ.size();
-                for (int i = 0; i < qsize; i++) {
-                    auto cur = GQ.front();
-                    GQ.pop();
-                    if (dist[cur.first][cur.second] >= ans) complete = true;
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur.first + dx[dir];
-                        int ny = cur.second + dy[dir];
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                        if (vis[nx][ny]) continue;
-                        if (dist[nx][ny]) continue;
-                        if (board[nx][ny] == 1 || dist[cur.first][cur.second] + 1 > ans) {
-                            complete = true;
-                            ans = min(ans, dist[cur.first][cur.second]);
-                        }
-                        dist[nx][ny] = dist[cur.first][cur.second] + 1;
-                        GQ.push({nx, ny});
-                    }
-                }
-                if (complete) break;
-            }
+        }
+    }
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    dist[i][j] = 0;
-                }
+    for (int i = 0; i < N; i++) {
+        fill(dist[i], dist[i] + N, -1);
+    }
+
+    queue<pair<int, int>> Q;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j]) {
+                dist[i][j] = 0;
+                Q.push({i, j});
             }
         }
     }
+
+    int ans = 987654321;
+
+    while (!Q.empty()) {
+        auto cur = Q.front();
+        Q.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (board[cur.first][cur.second] == board[nx][ny]) continue;
+
+            if (board[nx][ny]) {
+                ans = min(ans, dist[nx][ny] + dist[cur.first][cur.second]);  // 각각의 섬에 떨어진 위치를 의 합
+                continue;
+            }
+            board[nx][ny] = board[cur.first][cur.second];
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
+            Q.push({nx, ny});
+        }
+    }
+
     cout << ans;
 }

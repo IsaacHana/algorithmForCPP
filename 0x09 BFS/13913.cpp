b95b6a978335381bb4e2cfae1b6a878f02728388
IsaacHana <isaachan233@gmail.com>
2024-01-23 15:57:11 +0900

    13913 문제 풀기 (2024. 01. 23)

--- 0x09 BFS/13913.cpp
+++ 0x09 BFS/13913.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dist[100001];
-int prv[100001];
+const int MX = 100'000;
+int dist[MX + 1];
+int previous[MX + 1];
 
 int main() {
     ios::sync_with_stdio(0);
@@ -15,29 +16,32 @@ int main() {
     Q.push(N);
     dist[N] = 1;
 
-    while (dist[K] == 0) {
+    cout << '\n';
+    while (!dist[K]) {
         auto cur = Q.front();
         Q.pop();
 
-        for (int nxt : {cur - 1, cur + 1, cur * 2}) {
-            if (nxt < 0 || nxt > 100000) continue;
+        for (auto nxt : {cur - 1, cur + 1, cur << 1}) {
+            if (nxt < 0 || nxt > MX) continue;
             if (dist[nxt]) continue;
 
             dist[nxt] = dist[cur] + 1;
-            prv[nxt] = cur;
+            previous[nxt] = cur;
             Q.push(nxt);
         }
     }
 
     cout << dist[K] - 1 << '\n';
 
-    int i = K;
     stack<int> S;
-    S.push(i);
-    while (i != N) {
-        S.push(prv[i]);
-        i = prv[i];
+    S.push(K);
+    int cur = K;
+
+    while (cur != N) {
+        cur = previous[cur];
+        S.push(cur);
     }
+
     while (!S.empty()) {
         cout << S.top() << " ";
         S.pop();

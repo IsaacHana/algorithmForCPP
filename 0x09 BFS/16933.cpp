8d52182e9fe0fa912e28c04a0218a28d71ece9e3
IsaacHana <isaachan233@gmail.com>
2024-01-24 15:15:29 +0900

    16933 문제 복습(2024. 01. 24)

--- 0x09 BFS/16933.cpp
+++ 0x09 BFS/16933.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 987654321;
-
-string board[1001];
+const int MX = 987654321;
 int dist[2][11][1001][1001];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -14,6 +13,7 @@ int main() {
     int N, M, K;
     cin >> N >> M >> K;
 
+    string board[N];
     for (int i = 0; i < N; i++) {
         cin >> board[i];
     }
@@ -23,8 +23,8 @@ int main() {
     dist[1][K][0][0] = 1;
 
     while (!Q.empty()) {
-        int isBright, k, x, y;
-        tie(isBright, k, x, y) = Q.front();
+        int light, k, x, y;
+        tie(light, k, x, y) = Q.front();
         Q.pop();
 
         for (int dir = 0; dir < 4; dir++) {
@@ -33,34 +33,34 @@ int main() {
 
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-            if (board[nx][ny] == '1') {  // 벽
-                if (k < 1) continue;     // 횟수 전부 사용했거나 방분했다면 넘겨
-                if (!isBright) {         // 밤이면 기다려봐
-                    if (dist[!isBright][k][x][y]) continue;
-                    dist[!isBright][k][x][y] = dist[isBright][k][x][y] + 1;
-                    Q.push({!isBright, k, x, y});
+            if (board[nx][ny] == '1') {
+                if (k <= 0) continue;
+                if (light) {
+                    if (dist[!light][k - 1][nx][ny]) continue;
+                    dist[!light][k - 1][nx][ny] = dist[light][k][x][y] + 1;
+                    Q.push({!light, k - 1, nx, ny});
                 } else {
-                    if (dist[!isBright][k - 1][nx][ny]) continue;
-                    dist[!isBright][k - 1][nx][ny] = dist[isBright][k][x][y] + 1;
-                    Q.push({!isBright, k - 1, nx, ny});
+                    if (dist[!light][k][x][y]) continue;
+                    dist[!light][k][x][y] = dist[light][k][x][y] + 1;
+                    Q.push({!light, k, x, y});
                 }
-            } else {  // 벽X
-                if (dist[!isBright][k][nx][ny]) continue;
-                dist[!isBright][k][nx][ny] = dist[isBright][k][x][y] + 1;
-                Q.push({!isBright, k, nx, ny});
+            } else {
+                if (dist[!light][k][nx][ny]) continue;
+                dist[!light][k][nx][ny] = dist[light][k][x][y] + 1;
+                Q.push({!light, k, nx, ny});
             }
         }
     }
 
-    int ans = INF;
+    int ans = MX;
 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i <= 1; i++) {
         for (int j = 0; j <= K; j++) {
             if (dist[i][j][N - 1][M - 1]) ans = min(ans, dist[i][j][N - 1][M - 1]);
         }
     }
 
-    if (ans == INF)
+    if (ans == MX)
         cout << -1;
     else
         cout << ans;

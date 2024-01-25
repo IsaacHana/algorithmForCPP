8b5943cc0267b315a7b523de9050d7f23a32b446
IsaacHana <isaachan233@gmail.com>
2024-01-25 15:11:03 +0900

    11967 문제 복습 (2024. 01. 25)

--- 0x09 BFS/11967.cpp
+++ 0x09 BFS/11967.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[101][101];
 bool light[101][101];
+bool vis[101][101];
+
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -13,47 +14,53 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    vector<pair<int, int>> adj[101][101];
-
+    queue<pair<int, int>> adj[N + 1][N + 1];
     while (M--) {
-        int x, y, a, b;
-        cin >> x >> y >> a >> b;
-        adj[x][y].push_back({a, b});
+        int x, y, sx, sy;
+        cin >> x >> y >> sx >> sy;
+
+        adj[x][y].push({sx, sy});
     }
 
     queue<pair<int, int>> Q;
-    light[1][1] = 1;
-    board[1][1] = 1;
     Q.push({1, 1});
+    light[1][1] = true;
+    vis[1][1] = true;
 
     while (!Q.empty()) {
-        auto cur = Q.front();
+        int x, y;
+        tie(x, y) = Q.front();
         Q.pop();
 
-        for (auto neighbor : adj[cur.first][cur.second]) {
-            if (board[neighbor.first][neighbor.second]) continue;
+        while (!adj[x][y].empty()) {
+            auto cur = adj[x][y].front();
+            adj[x][y].pop();
+            if (vis[cur.first][cur.second]) continue;
+
+            light[cur.first][cur.second] = true;
+
             for (int dir = 0; dir < 4; dir++) {
-                int nx = neighbor.first + dx[dir];
-                int ny = neighbor.second + dy[dir];
+                int nx = cur.first + dx[dir];
+                int ny = cur.second + dy[dir];
 
                 if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
-                if (!board[nx][ny]) continue;
-                board[nx][ny] = 1;
-                Q.push({nx, ny});
-                break;
+                if (vis[nx][ny]) {
+                    vis[cur.first][cur.second] = true;
+                    Q.push({cur.first, cur.second});
+                    break;
+                }
             }
-            light[neighbor.first][neighbor.second] = true;
         }
 
         for (int dir = 0; dir < 4; dir++) {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
 
             if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
             if (!light[nx][ny]) continue;
-            if (board[nx][ny]) continue;
+            if (vis[nx][ny]) continue;
 
-            board[nx][ny] = 1;
+            vis[nx][ny] = true;
             Q.push({nx, ny});
         }
     }
@@ -61,11 +68,8 @@ int main() {
     int ans = 0;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            // cout << light[i][j] << " ";
-            ans += light[i][j];
+            if (light[i][j]) ans++;
         }
-        // cout << "\n";
     }
-
     cout << ans;
 }

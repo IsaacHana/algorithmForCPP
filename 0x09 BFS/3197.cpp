01d9b5b562602f10ddfd888425b401ceb7d184ae
IsaacHana <isaachan233@gmail.com>
2024-01-27 17:08:38 +0900

    3197 문제 풀기 다시 풀것 (2024. 01. 27)

--- 0x09 BFS/3197.cpp
+++ 0x09 BFS/3197.cpp
@@ -1,84 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char board[1501][1501];
-bool vis1[1501][1501];
-bool vis2[1501][1501];
 int R, C;
-int ans = 0;
-pair<int, int> S;
-queue<pair<int, int>> CQ;
-queue<pair<int, int>> CQ2;
-queue<pair<int, int>> MQ;
-queue<pair<int, int>> MQ2;
+
+queue<pair<int, int>> bird;
+queue<pair<int, int>> bird_cache;
+queue<pair<int, int>> water;
+queue<pair<int, int>> water_cache;
+
+char board[1501][1501];
+bool vis_bird[1501][1501];
+bool vis_water[1501][1501];
+
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
 bool isConnect() {
-    // 1. 백조 만나는지
-    while (!CQ.empty()) {
-        int x, y;
-        tie(x, y) = CQ.front();
-        CQ.pop();
+    while (!bird.empty()) {
+        auto cur = bird.front();
+        bird.pop();
 
         for (int dir = 0; dir < 4; dir++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
 
             if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-            if (vis1[nx][ny]) continue;
-
+            if (vis_bird[nx][ny]) continue;
+            vis_bird[nx][ny] = true;
             if (board[nx][ny] == 'L') {
                 return true;
-                break;
             } else if (board[nx][ny] == 'X') {
-                vis1[nx][ny] = true;
-                CQ2.push({nx, ny});
-                continue;
+                bird_cache.push({nx, ny});
+            } else {
+                bird.push({nx, ny});
             }
-
-            vis1[nx][ny] = true;
-            CQ.push({nx, ny});
         }
     }
 
-    while (!CQ2.empty()) {
-        int x, y;
-        tie(x, y) = CQ2.front();
-        CQ2.pop();
-        CQ.push({x, y});
+    while (!bird_cache.empty()) {
+        auto cur = bird_cache.front();
+        bird_cache.pop();
+
+        bird.push({cur.first, cur.second});
     }
 
     return false;
 }
 
-// 2.빙하 녹이기
 void melting() {
-    while (!MQ.empty()) {
-        int x, y;
-        tie(x, y) = MQ.front();
-        MQ.pop();
-        vis2[x][y] = true;
+    while (!water.empty()) {
+        auto cur = water.front();
+        water.pop();
 
         for (int dir = 0; dir < 4; dir++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
 
             if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-            if (vis2[nx][ny]) continue;
+            if (vis_water[nx][ny]) continue;
+
             if (board[nx][ny] == 'X') {
-                MQ2.push({nx, ny});
+                water_cache.push({nx, ny});
             }
-            vis2[nx][ny] = true;
+            vis_water[nx][ny] = true;
         }
     }
 
-    while (!MQ2.empty()) {
-        int x, y;
-        tie(x, y) = MQ2.front();
-        MQ2.pop();
-        board[x][y] = '.';
-        MQ.push({x, y});
+    while (!water_cache.empty()) {
+        auto cur = water_cache.front();
+        water_cache.pop();
+        board[cur.first][cur.second] = '.';
+        water.push({cur.first, cur.second});
     }
 }
 
@@ -86,31 +78,33 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 1. 백조 만날 수 있는지
-    // 2. 빙판 녹이기
-
     cin >> R >> C;
+
+    char c;
+    pair<int, int> bird_start;
 
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
-            cin >> board[i][j];
-            if (board[i][j] == 'L') {
-                S = {i, j};
+            cin >> c;
+            board[i][j] = c;
+            if (c == 'L') {
+                bird_start = {i, j};
             }
-            if (board[i][j] != 'X') {
-                MQ.push({i, j});
+            if (c != 'X') {
+                water.push({i, j});
+                vis_water[i][j] = true;
             }
         }
     }
 
-    CQ.push(S);
-    board[S.first][S.second] = '.';
-    vis1[S.first][S.second] = true;
-    // 3. 갯수 세기
+    bird.push(bird_start);
+    board[bird_start.first][bird_start.second] = '.';
+    vis_bird[bird_start.first][bird_start.second] = true;
+
+    int ans = 0;
     while (!isConnect()) {
         melting();
         ans++;
     }
-
-    cout << ans << '\n';
+    cout << ans;
 }

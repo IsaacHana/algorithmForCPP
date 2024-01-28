67a2f7473c3f4f894dc0ed2992178663ccd64ac0
IsaacHana <isaachan233@gmail.com>
2024-01-28 22:54:37 +0900

    1629 1회자 문제 풀기

---
+++ 0x10 재귀/1629.cpp
@@ -0,0 +1,18 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    long long POW(long long A, long long B, long long C) {
+        if (B == 1) return A % C;
+        long long tmp = POW(A, B / 2, C);
+        tmp = tmp * tmp % C;
+        if (B % 2 == 0) return tmp;
+        return tmp * A % C;
+    }
+
+    int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        long long A, B, C;
+        cin >> A >> B >> C;
+        cout << POW(A, B, C);
+    }

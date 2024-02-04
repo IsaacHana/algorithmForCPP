8c9cd7db92554e12710625e37a2e7960baa14314
IsaacHana <isaachan233@gmail.com>
2024-02-04 14:55:07 +0900

    1074 문제 복습 (2024. 02. 04)

--- 0x10 재귀/1074.cpp
+++ 0x10 재귀/1074.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int recursion(int n, int r, int c) {
+int recursion(int N, int x, int y) {
     // base case
-    if (n == 0) return 0;
-    int half = 1 << (n - 1);
-    if (r < half && c < half) return recursion(n - 1, r, c);                            // 2사분면
-    if (r < half && c >= half) return half * half + recursion(n - 1, r, c - half);      // 1사분면
-    if (r >= half && c < half) return 2 * half * half + recursion(n - 1, r - half, c);  // 3사분면
-    return 3 * half * half + recursion(n - 1, r - half, c - half);                      // 4사분면
-}
+    if (N == 0) return 0;
+    int half = 1 << (N - 1);
 
+    if (x < half && y < half) return recursion(N - 1, x, y);
+
+    if (x < half && y >= half) return half * half + recursion(N - 1, x, y - half);
+
+    if (x >= half && y < half) return 2 * half * half + recursion(N - 1, x - half, y);
+
+    if (x >= half && y >= half) return 3 * half * half + recursion(N - 1, x - half, y - half);
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int N, r, c;
     cin >> N >> r >> c;
     cout << recursion(N, r, c);

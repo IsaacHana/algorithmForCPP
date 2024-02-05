870393f91253ac465f645111f27b58478ef1f83e
IsaacHana <isaachan233@gmail.com>
2024-02-05 11:41:07 +0900

    2448 문제 복습 (2024. 02. 05)

--- 0x10 재귀/2448.cpp
+++ 0x10 재귀/2448.cpp
@@ -1,28 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 3 * 1024;
-char board[MX + 1][MX * 2 - 1];
+const int MX = 1024 * 3;
+char board[MX][2 * MX - 1];
 
-void stars(int x, int y) {
+void stars(int N, int x, int y) {
     board[x][y] = '*';
-
     board[x + 1][y - 1] = '*';
     board[x + 1][y + 1] = '*';
-
     for (int i = y - 2; i <= y + 2; i++) {
         board[x + 2][i] = '*';
     }
 }
 
 void recursion(int N, int x, int y) {
+    // base case
     if (N == 3) {
-        stars(x, y);
+        stars(N, x, y);
         return;
     }
-
     int n = N / 2;
-
     recursion(n, x, y);
     recursion(n, x + n, y - n);
     recursion(n, x + n, y + n);
@@ -31,18 +28,14 @@ void recursion(int N, int x, int y) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int N;
     cin >> N;
-    // 1. 가운데 빈 삼각형을 만들 수 있다.
-    // stars(0, N - 1);
-    // 2. 나머지 부분도 다 1. 처럼 할 수 있다.
-    // stars(0 + N / 2, N - 1 - N / 2);
-    // stars(0 + N / 2, N - 1 + N / 2);
-
     for (int i = 0; i < N; i++) {
         fill(board[i], board[i] + 2 * N - 1, ' ');
     }
+
+    // stars(3, 0, 2);
+    // stars(N, 0, N - 1);
 
     recursion(N, 0, N - 1);
 

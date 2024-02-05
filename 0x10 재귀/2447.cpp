4131bd3360d06bfb6a23df5af9754b34b4e4b1a9
IsaacHana <isaachan233@gmail.com>
2024-02-05 11:20:43 +0900

    2447 문제 복습 (2024. 02. 05)

--- 0x10 재귀/2447.cpp
+++ 0x10 재귀/2447.cpp
@@ -1,27 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 1 3 9 27 81 273 819 2457 7371
-bool board[2458][2458];
-
-void stars(int n, int x, int y) {
-    for (int i = x + n; i < x + 2 * n; i++) {
-        for (int j = y + n; j < y + 2 * n; j++) {
-            board[i][j] = 1;
-        }
-    }
-}
+char board[2460][2460];
 
 void recursion(int N, int x, int y) {
-    int n = N / 3;
-
-    stars(n, x, y);
-
-    if (n == 1) {
+    // base case
+    if (N == 1) {
+        board[x][y] = '*';
         return;
     }
 
+    int n = N / 3;
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
+            if (i == 1 && j == 1) continue;
             recursion(n, x + i * n, y + j * n);
         }
     }
@@ -30,22 +22,18 @@ void recursion(int N, int x, int y) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int N;
     cin >> N;
 
-    // stars(3, 0, 0);
-    // stars(3, 3, 0);
-    // stars(9, 0, 0);
+    for (int i = 0; i < N; i++) {
+        fill(board[i], board[i] + N, ' ');
+    }
 
     recursion(N, 0, 0);
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (!board[i][j])
-                cout << '*';
-            else
-                cout << ' ';
+            cout << board[i][j];
         }
         cout << '\n';
     }

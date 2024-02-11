6952fba687d018c0d44d7f074792540ca2229436
IsaacHana <isaachan233@gmail.com>
2024-02-11 17:27:01 +0900

    11403 문제 풀기

---
+++ etc/11403.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int adj[101][101];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> adj[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                if (adj[j][i] && adj[i][k]) adj[j][k] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << adj[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}

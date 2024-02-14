a422b81c1c18db275df3439a55e3e55784451972
IsaacHana <isaachan233@gmail.com>
2024-02-14 13:29:09 +0900

    9663 문제 복습 (2024. 02. 14)

--- 0x11 백트레킹/9663.cpp
+++ 0x11 백트레킹/9663.cpp
@@ -1,36 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool vis1[15];
-bool vis2[30];
-bool vis3[30];
 int N;
-int cnt = 0;
 
-void backTracking(int depth) {
+int cnt = 0;
+int vis_row[15];
+int vis_up_cross[29];  // 2 * N - 1;
+int vis_down_cross[29];
+
+void dfs(int depth) {
+    // base
     if (depth == N) {
         cnt++;
         return;
     }
 
     for (int i = 0; i < N; i++) {
-        if (vis1[i] || vis2[i + depth] || vis3[depth - i + N - 1]) continue;
-        vis1[i] = true;
-        vis2[i + depth] = true;
-        vis3[depth - i + N - 1] = true;
-        backTracking(depth + 1);
+        if (vis_row[i]) continue;
+        if (vis_up_cross[i + depth]) continue;
+        if (vis_down_cross[depth - i + N - 1]) continue;
 
-        vis1[i] = false;
-        vis2[i + depth] = false;
-        vis3[depth - i + N - 1] = false;
+        vis_row[i] = true;
+        vis_up_cross[i + depth] = true;
+        vis_down_cross[depth - i + N - 1] = true;
+
+        dfs(depth + 1);
+
+        vis_row[i] = false;
+        vis_up_cross[i + depth] = false;
+        vis_down_cross[depth - i + N - 1] = false;
     }
-};
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> N;
 
-    backTracking(0);
+    cin >> N;
+    dfs(0);
     cout << cnt;
 }

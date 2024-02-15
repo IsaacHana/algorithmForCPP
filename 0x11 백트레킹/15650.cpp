98b25eb75dd6c8159868ea1c965f3dacc7b5492c
IsaacHana <isaachan233@gmail.com>
2024-02-15 14:48:36 +0900

    15650 문제 복습 (2024. 02. 15)

--- 0x11 백트레킹/15650.cpp
+++ 0x11 백트레킹/15650.cpp
@@ -2,33 +2,17 @@
 using namespace std;
 
 int N, M;
-int arr[9];
-bool vis[9];
-
-void backTracking(int depth) {
-    if (depth == M) {
-        for (int i = 0; i < M; i++) {
-            cout << arr[i] << ' ';
-        }
-        cout << '\n';
-        return;
-    }
-
-    int start = 1;
-    if (depth != 0) start = arr[depth - 1] + 1;
-    for (int i = start; i <= N; i++) {
-        if (!vis[i]) {
-            arr[depth] = i;
-            vis[i] = true;
-            backTracking(depth + 1);
-            vis[i] = false;
-        }
-    }
-}
+vector<int> v;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> N >> M;
 
-    backTracking(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) v.push_back(i < M ? 0 : 1);
+    // for (int i = 0; i < N; i++) cout << v[i] << ' ';
+    do {
+        for (int i = 0; i < N; i++)
+            if (v[i] == 0) cout << i + 1 << ' ';
+        cout << '\n';
+    } while (next_permutation(v.begin(), v.end()));
 }

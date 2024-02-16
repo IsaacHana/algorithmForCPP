5734b8837bd4a86b05d39a6c420552a1a497aa6a
IsaacHana <isaachan233@gmail.com>
2024-02-16 10:51:49 +0900

    15655 문제 복습 (2024. 02. 16)

--- 0x11 백트레킹/15655.cpp
+++ 0x11 백트레킹/15655.cpp
@@ -2,28 +2,22 @@
 using namespace std;
 
 int N, M;
-int numbers[9];
-int arr[9];
-bool vis[9];
 
-void backTracking(int depth) {
-    // base case
+int num[10];
+int arr[10];
+
+void dfs(int depth, int start) {
     if (depth == M) {
         for (int i = 0; i < M; i++) {
-            cout << numbers[arr[i]] << ' ';
+            cout << arr[i] << ' ';
         }
         cout << '\n';
         return;
     }
-    int start = 0;
-    if (depth != 0) start = arr[depth - 1] + 1;
+
     for (int i = start; i < N; i++) {
-        if (!vis[i]) {
-            arr[depth] = i;
-            vis[i] = true;
-            backTracking(depth + 1);
-            vis[i] = false;
-        }
+        arr[depth] = num[i];
+        dfs(depth + 1, i + 1);
     }
 }
 
@@ -31,9 +25,8 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        cin >> numbers[i];
-    }
-    sort(numbers, numbers + N);
-    backTracking(0);
+
+    for (int i = 0; i < N; i++) cin >> num[i];
+    sort(num, num + N);
+    dfs(0, 0);
 }

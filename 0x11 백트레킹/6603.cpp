6e82f852f02171e1a8ac087c263222f35180612c
IsaacHana <isaachan233@gmail.com>
2024-02-17 14:48:24 +0900

    6603 문제 복습 (2024. 02. 17)

--- 0x11 백트레킹/6603.cpp
+++ 0x11 백트레킹/6603.cpp
@@ -3,41 +3,33 @@ using namespace std;
 
 const int MIN_NUM = 1;
 const int MAX_NUM = 49;
-const int CNT = 6;
+const int M = 6;
 
-int k;
-int num[14];
-int arr[14];
+int N;
+int num[51];
+int arr[M];
 
-void backTracking(int depth, int start) {
-    // base case
-    if (depth == CNT) {
-        for (int i = 0; i < CNT; i++) {
-            cout << arr[i] << ' ';
-        }
+void dfs(int depth, int start) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) cout << arr[i] << ' ';
         cout << '\n';
         return;
     }
 
-    for (int i = start; i < k; i++) {
+    for (int i = start; i < N; i++) {
         arr[depth] = num[i];
-        backTracking(depth + 1, i + 1);
+        dfs(depth + 1, i + 1);
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     while (true) {
-        cin >> k;
-        if (k == 0) break;
-
-        for (int i = 0; i < k; i++) {
-            cin >> num[i];
-        }
-
-        backTracking(0, 0);
+        cin >> N;
+        if (N == 0) break;
+        for (int i = 0; i < N; i++) cin >> num[i];
+        dfs(0, 0);
         cout << '\n';
     }
 }

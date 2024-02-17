4f645d4b466d3f21a87f4a02decac98ad739656c
IsaacHana <isaachan233@gmail.com>
2024-02-17 14:48:45 +0900

    15665 문제 복습 (2024. 02. 17)

--- 0x11 백트레킹/15665.cpp
+++ 0x11 백트레킹/15665.cpp
@@ -1,38 +1,31 @@
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int N, M;
-int numbers[8];
-int arr[8];
-
-void backTracking(int depth) {
-    // base case
+int num[10];
+int arr[10];
+void dfs(int depth) {
     if (depth == M) {
-        for (int i = 0; i < M; i++) {
-            cout << arr[i] << ' ';
-        }
+        for (int i = 0; i < M; i++) cout << arr[i] << ' ';
         cout << '\n';
         return;
     }
-
     int tmp = 0;
     for (int i = 0; i < N; i++) {
-        if (tmp == numbers[i]) continue;
-        arr[depth] = numbers[i];
+        if (tmp == num[i]) continue;
+        arr[depth] = num[i];
         tmp = arr[depth];
-        backTracking(depth + 1);
+        dfs(depth + 1);
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> N >> M;
     for (int i = 0; i < N; i++) {
-        cin >> numbers[i];
+        cin >> num[i];
     }
-    sort(numbers, numbers + N);
-    backTracking(0);
+    sort(num, num + N);
+    dfs(0);
 }

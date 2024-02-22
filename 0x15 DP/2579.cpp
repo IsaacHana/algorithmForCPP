53a0ccd0feccd9111e6d87f1c45c7ea1b5abd24b
IsaacHana <isaachan233@gmail.com>
2024-02-22 22:16:14 +0900

    2579 문제 풀기

--- 0x15 DP/2579.cpp
+++ 0x15 DP/2579.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int stairs[302];
-int DP[302];
+int dp[302];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     int N;
     cin >> N;
-    for (int i = 0; i < N; i++) {
+
+    for (int i = 1; i <= N; i++) {
         cin >> stairs[i];
     }
 
-    DP[0] = stairs[0];
-    DP[1] = max(stairs[0] + stairs[1], stairs[1]);
-    DP[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+    dp[1] = stairs[1];
+    dp[2] = max(stairs[1] + stairs[2], stairs[2]);
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
 
-    for (int i = 3; i < N; i++) {
-        DP[i] = max(DP[i - 2] + stairs[i], DP[i - 3] + stairs[i - 1] + stairs[i]);
+    for (int i = 4; i <= N; i++) {
+        dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
     }
 
-    cout << DP[N - 1] << '\n';
+    cout << dp[N] << '\n';
 }

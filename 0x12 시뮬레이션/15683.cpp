4eeb434118f5f061c1b1d73f0174a6f0fe0b7f1e
IsaacHana <isaachan233@gmail.com>
2024-02-26 20:14:35 +0900

    15684 문제 복습 (2024. 02 .26) 4진법 대신 백트래킹으로 구현

--- 0x12 시뮬레이션/15683.cpp
+++ 0x12 시뮬레이션/15683.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 
 int N, M;
+int board[21][21];
+int copy_board[21][21];
 
-int board[10][10];
-int copy_board[10][10];
+struct Cctv {
+    int x;
+    int y;
+    int model;
+    int direct;
+};
 
-vector<pair<int, int>> cctv;
+vector<Cctv> cctvs;
+vector<int> arr;
+int dir[4] = {0, 1, 2, 3};
+int cctv_size;
+bool vis[4];
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+
+int mx = 0;
 
 void watch(int x, int y, int dir) {
     dir %= 4;
@@ -19,79 +31,96 @@ void watch(int x, int y, int dir) {
         if (x < 0 || x >= N || y < 0 || y >= M) return;
         if (copy_board[x][y] == 6) return;
         if (copy_board[x][y] != 0) continue;
-
         copy_board[x][y] = 7;
     }
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> N >> M;
-
-    int ans = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> board[i][j];
-
-            if (board[i][j] == 0) {
-                ans++;
-                continue;
-            }
-            if (board[i][j] == 6) continue;
-            cctv.push_back({i, j});
-        }
-    }
-
-    for (int cur = 0; cur < 1 << (2 * cctv.size()); cur++) {
+void dfs(int depth) {
+    if (depth == cctv_size) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 copy_board[i][j] = board[i][j];
             }
         }
 
-        int brute = cur;
+        for (int i = 0; i < cctv_size; i++) {
+            cctvs[i].direct = arr[i];
+            // cout << arr[i] << ' ';
+        }
+        // cout << '\n';
 
-        for (int i = 0; i < cctv.size(); i++) {
-            int dir = brute % 4;
-            brute /= 4;
-
-            int x = cctv[i].first;
-            int y = cctv[i].second;
-
-            if (board[x][y] == 1) {
-                watch(x, y, dir);
-            } else if (board[x][y] == 2) {
-                watch(x, y, dir);
-                watch(x, y, dir + 2);
-            } else if (board[x][y] == 3) {
-                watch(x, y, dir);
-                watch(x, y, dir + 1);
-            } else if (board[x][y] == 4) {
-                watch(x, y, dir);
-                watch(x, y, dir + 1);
-                watch(x, y, dir + 2);
-            } else {
-                watch(x, y, dir);
-                watch(x, y, dir + 1);
-                watch(x, y, dir + 2);
-                watch(x, y, dir + 3);
+        for (Cctv cctv : cctvs) {
+            int dir = cctv.direct;
+            if (cctv.model == 1) {
+                watch(cctv.x, cctv.y, cctv.direct);
+            } else if (cctv.model == 2) {
+                watch(cctv.x, cctv.y, cctv.direct);
+                watch(cctv.x, cctv.y, cctv.direct + 2);
+            } else if (cctv.model == 3) {
+                watch(cctv.x, cctv.y, cctv.direct);
+                watch(cctv.x, cctv.y, cctv.direct + 1);
+            } else if (cctv.model == 4) {
+                watch(cctv.x, cctv.y, cctv.direct);
+                watch(cctv.x, cctv.y, cctv.direct + 1);
+                watch(cctv.x, cctv.y, cctv.direct + 2);
+            } else if (cctv.model == 5) {
+                watch(cctv.x, cctv.y, cctv.direct);
+                watch(cctv.x, cctv.y, cctv.direct + 1);
+                watch(cctv.x, cctv.y, cctv.direct + 2);
+                watch(cctv.x, cctv.y, cctv.direct + 3);
             }
         }
-
         int cnt = 0;
-        // cout << '\n';
+        // for (int i = 0; i < N; i++) {
+        //     for (int j = 0; j < M; j++) {
+        //         cout << copy_board[i][j] << ' ';
+        //     }
+        //     cout << '\n';
+        // }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // cout << copy_board[i][j] << ' ';
-                if (copy_board[i][j] == 0) {
-                    cnt += 1;
-                }
+                if (copy_board[i][j] == 0) cnt++;
             }
-            // cout << '\n';
         }
-        // cout << cnt << '\n';
-        ans = min(ans, cnt);
+        mx = min(mx, cnt);
+        return;
     }
-    cout << ans << '\n';
+
+    for (int i = 0; i < 4; i++) {
+        arr.push_back(dir[i]);
+        dfs(depth + 1);
+        arr.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> board[i][j];
+
+            if (board[i][j] == 0) {
+                mx++;
+                continue;
+            }
+
+            if (board[i][j] == 6) continue;
+
+            Cctv cctv;
+            cctv.x = i;
+            cctv.y = j;
+            cctv.model = board[i][j];
+            cctvs.push_back(cctv);
+        }
+    }
+
+    cctv_size = cctvs.size();
+
+    dfs(0);
+
+    cout << mx << '\n';
 }

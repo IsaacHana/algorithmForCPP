385a378c15a28e06a3994fc64c04a72d0517896d
IsaacHana <isaachan233@gmail.com>
2024-02-27 14:31:21 +0900

    1799 문제 복습 (2024. 02 .27)

--- 0x11 백트레킹/1799.cpp
+++ 0x11 백트레킹/1799.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
 vector<pair<int, int>> board[2][20];
-bool vis[2][20];
+int N;
+int vis[20];
 int ans[2];
-void backtracking(int depth, int cnt, int color) {
+
+void dfs(int depth, int cnt, int color) {
+    // 가지 치기
+    if (ans[color] >= cnt + N - 1) {
+        return;
+    }
+
     // base case
     if (depth == 2 * N - 1) {
         ans[color] = max(ans[color], cnt);
         return;
     }
 
-    for (auto v : board[color][depth]) {
+    for (auto cur : board[color][depth]) {
         int x, y;
-        tie(x, y) = v;
-        if (vis[color][x + y]) continue;
-        vis[color][x + y] = true;
-        backtracking(depth + 1, cnt + 1, color);
-        vis[color][x + y] = false;
+        tie(x, y) = cur;
+        if (vis[x + y]) continue;
+        vis[x + y] = true;
+        dfs(depth + 1, cnt + 1, color);
+        vis[x + y] = false;
     }
-    backtracking(depth + 1, cnt, color);
+
+    dfs(depth + 1, cnt, color);
 }
 
 int main() {
@@ -29,14 +36,15 @@ int main() {
 
     cin >> N;
     int val;
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cin >> val;
-            if (val) board[(i + j) % 2][N + i - j - 1].push_back({i, j});
+            if (val) board[(i + j) % 2][N - 1 + i - j].push_back({i, j});
         }
     }
 
-    backtracking(0, 0, 0);
-    backtracking(0, 0, 1);
+    dfs(0, 0, 0);
+    dfs(0, 0, 1);
+
     cout << ans[0] + ans[1] << '\n';
 }

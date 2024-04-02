3593fc5ae35e1cead456e688e22958684762c021
IsaacHana <isaachan233@gmail.com>
2024-04-02 23:17:17 +0900

    17141 문제 풀기

--- 0x12 시뮬레이션/17141.cpp
+++ 0x12 시뮬레이션/17141.cpp
@@ -1,71 +1,112 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
+int N, M;
+int board[52][52];
+vector<pair<int, int>> vec;
+vector<pair<int, int>> vir;
 
-const int MX = 0x7f7f7f7f;
-int n, m;
-int board[55][55];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-vector<pair<int, int>> v;
-queue<pair<int, int>> q;
-int ans = MX;
-int dist[55][55];
 
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> board[i][j];
-            if (board[i][j] == 2) {
-                v.push_back({i, j});
-                board[i][j] = 0;
-            }
+const int MAX = 0x7f7f7f7f;
+int ans = MAX;
+
+void bfs() {
+    int dist[52][52];
+
+    for (int i = 0; i <= N + 1; i++) {
+        for (int j = 0; j <= N + 1; j++) {
+            dist[i][j] = -1;
         }
     }
 
-    vector<int> brute(v.size());
-    for (int i = m; i < v.size(); i++) brute[i] = 1;
+    queue<pair<int, int>> q;
 
-    do {
-        int t = 0;
-        for (int i = 0; i < n; i++) fill(dist[i], dist[i] + n, -1);
+    for (auto v : vir) {
+        dist[v.first][v.second] = 0;
+        q.push(v);
+    }
 
-        for (int i = 0; i < v.size(); i++) {
-            if (!brute[i]) {
-                q.push({v[i].X, v[i].Y});
-                dist[v[i].X][v[i].Y] = 0;
-            }
+    int mx = 0;
+
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+            if (board[nx][ny] == 1) continue;
+            if (dist[nx][ny] != -1) continue;
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
+            if (mx >= ans) return;
+            mx = max(mx, dist[nx][ny]);
+            q.push({nx, ny});
         }
+    }
 
-        while (!q.empty()) {
-            auto cur = q.front();
-            q.pop();
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur.X + dx[dir];
-                int ny = cur.Y + dy[dir];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if (board[nx][ny] != 0 || dist[nx][ny] >= 0) continue;
-                dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-                t = max(t, dist[nx][ny]);
-                q.push({nx, ny});
-            }
+    bool flag = true;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            if ((board[i][j] == 0 || board[i][j] == 2) && dist[i][j] == -1) flag = false;
         }
+    }
 
-        bool flag = true;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (board[i][j] == 0 && dist[i][j] == -1) flag = false;
+    if (flag) ans = min(ans, mx);
 
-        if (flag) ans = min(ans, t);
-    } while (next_permutation(brute.begin(), brute.end()));
+    // for (int i = 0; i <= N + 1; i++) {
+    //     for (int j = 0; j <= N + 1; j++) {
+    //         cout << dist[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+    // cout << "flag : " << flag << " mx: " << mx << " ans : " << ans << '\n';
+}
 
-    if (ans == MX)
-        cout << -1;
+void dfs(int depth, int start) {
+    if (depth == M) {
+        // for (auto v : vir) {
+        //     cout << v.first << " " << v.second << ", ";
+        // }
+        // cout << '\n';
+
+        bfs();
+
+        return;
+    }
+
+    for (int i = start; i < vec.size(); i++) {
+        auto v = vec[i];
+
+        vir.push_back(v);
+        dfs(depth + 1, i + 1);
+        vir.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i <= N + 1; i++) {
+        fill(board[i], board[i] + N + 2, 1);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 2) vec.push_back({i, j});
+        }
+    }
+
+    dfs(0, 0);
+
+    if (ans == MAX)
+        cout << -1 << '\n';
     else
-        cout << ans;
+        cout << ans << '\n';
 }

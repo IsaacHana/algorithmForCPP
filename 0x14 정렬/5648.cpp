705c6bcde35a9fa7303e4950f7a2f571156c33ec
IsaacHana <isaachan233@gmail.com>
2024-04-04 23:53:03 +0900

    5648 문제 풀기

---
+++ 0x14 정렬/5648.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> vec;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    string val;
+    while (n--) {
+        cin >> val;
+        reverse(val.begin(), val.end());
+        vec.push_back(stoll(val));
+    }
+
+    sort(vec.begin(), vec.end());
+
+    for (int v : vec) {
+        cout << v << '\n';
+    }
+}

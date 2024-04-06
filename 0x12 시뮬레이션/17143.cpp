50e436a8b7a984c93227e542b93ddcc0278dbc3a
IsaacHana <isaachan233@gmail.com>
2024-04-06 12:55:20 +0900

    17143 문제 해결

--- 0x12 시뮬레이션/17143.cpp
+++ 0x12 시뮬레이션/17143.cpp
@@ -12,8 +12,8 @@ int R, C, M;
 vector<shark> board[102][102];
 vector<shark> des[102][102];
 
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+int dx[5] = {0, -1, 1, 0, 0};
+int dy[5] = {0, 0, 0, 1, -1};
 
 void print_sharks() {
     for (int i = 1; i <= R; i++) {
@@ -34,37 +34,37 @@ void shark_move(int i, int j) {
     shark sh = board[i][j][0];
     board[i][j].pop_back();
 
-    cout << "현재 위치 : " << i << " " << j << '\n';
-    cout << "현재 상태 : " << sh.d << " " << sh.s << " " << sh.z << '\n';
+    // cout << "현재 위치 : " << i << " " << j << '\n';
+    // cout << "현재 상태 : " << sh.d << " " << sh.s << " " << sh.z << '\n';
 
     if (sh.d == 1 || sh.d == 2) {
-        int move = sh.s;
+        int move = sh.s % (2 * (R - 1));
 
         while (move--) {
-            i += dx[sh.d - 1];
-            j += dy[sh.d - 1];
+            i += dx[sh.d];
+            j += dy[sh.d];
             if (i <= 0 || i > R || j <= 0 || j > C) {
                 sh.d = 3 - sh.d;
                 i += 2 * dx[sh.d];
-                j += 2 * dx[sh.d];
+                j += 2 * dy[sh.d];
             }
         }
     } else {
-        int move = sh.s;
+        int move = sh.s % (2 * (C - 1));
 
         while (move--) {
-            i += dx[sh.d - 1];
-            j += dy[sh.d - 1];
+            i += dx[sh.d];
+            j += dy[sh.d];
             if (i <= 0 || i > R || j <= 0 || j > C) {
                 sh.d = 7 - sh.d;
                 i += 2 * dx[sh.d];
-                j += 2 * dx[sh.d];
+                j += 2 * dy[sh.d];
             }
         }
     }
 
-    cout << "목적지 " << i << " " << j << '\n';
-    cout << "목적지 상태 " << sh.d << " " << sh.s << " " << sh.z << '\n';
+    // cout << "목적지 " << i << " " << j << '\n';
+    // cout << "목적지 상태 " << sh.d << " " << sh.s << " " << sh.z << '\n';
 
     des[i][j].push_back(sh);
 }
@@ -85,14 +85,14 @@ void sharks_move() {
         }
     }
     // compare size
-    // for (int i = 1; i <= R; i++) {
-    //     for (int j = 1; j <= C; j++) {
-    //         if (board[i][j].size() > 1) {
-    //             sort(board[i][j].begin(), board[i][j].end(), [](auto& a, auto& b) { return a.z > b.z; });
-    //             while (board[i][j].size() > 1) board[i][j].pop_back();
-    //         }
-    //     }
-    // }
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            if (board[i][j].size() > 1) {
+                sort(board[i][j].begin(), board[i][j].end(), [](auto& a, auto& b) { return a.z > b.z; });
+                while (board[i][j].size() > 1) board[i][j].pop_back();
+            }
+        }
+    }
 }
 
 int cur_c = 0;
@@ -101,7 +101,7 @@ int total = 0;
 void move_fisher() { cur_c++; }
 
 void catch_shark() {
-    for (int i = 0; i < C; i++) {
+    for (int i = 1; i <= R; i++) {
         if (board[i][cur_c].empty()) continue;
         shark sh = board[i][cur_c].front();
         board[i][cur_c].pop_back();
@@ -123,7 +123,19 @@ int main() {
         board[r][c].push_back(sh);
     }
 
-    print_sharks();
-    sharks_move();
-    print_sharks();
+    // print_sharks();
+    // sharks_move();
+    // print_sharks();
+    // sharks_move();
+    // print_sharks();
+
+    for (int i = 0; i < C; i++) {
+        // print_sharks();
+        move_fisher();
+        catch_shark();
+        sharks_move();
+    }
+    // print_sharks();
+
+    cout << total << '\n';
 }

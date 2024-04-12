57de1d05ff2af7e7a19ca8161eec1597114bc8eb
IsaacHana <isaachan233@gmail.com>
2024-04-12 23:57:25 +0900

    10814 문제 풀기

---
+++ 0x14 정렬/10814.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<tuple<int, int, string>> members;
+
+    for (int i = 0; i < N; i++) {
+        int age;
+        string name;
+        cin >> age >> name;
+        members.push_back({age, i, name});
+    }
+
+    sort(members.begin(), members.end());
+
+    for (auto member : members) {
+        cout << get<0>(member) << ' ' << get<2>(member) << '\n';
+    }
+}

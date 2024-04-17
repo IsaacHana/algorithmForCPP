18d97e22ff04ab0625f22385b5ff6940af1b78c6
IsaacHana <isaachan233@gmail.com>
2024-04-17 23:50:22 +0900

    7795 문제 풀기

---
+++ 0x14 정렬/7795.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        vector<pair<int, int>> vec(n + m);
+
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+            vec[i] = {a, 0};
+        }
+
+        for (int i = n; i < n + m; i++) {
+            int b;
+            cin >> b;
+            vec[i] = {b, 1};
+        }
+
+        sort(vec.begin(), vec.end());
+
+        int cnt = 0;
+        int ans = 0;
+    
+        for (int i = 0; i < n + m; i++) {
+            if (vec[i].second == 0)
+                ans += cnt;
+            else
+                cnt++;
+        }
+
+        cout << ans << '\n';
+    }
+}
